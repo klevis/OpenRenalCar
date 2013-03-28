2a64da14f7f37b8fec01ea63b28a97f3198113e3
root <root@ramok-Vostro-200>
2013-03-28 15:20:06 +0100

    Show car Image

--- ramo.klevis.openrental.checkout/src/ramo/klevis/openrental/parts/checkout/PartSelectedCarInfo.java
+++ ramo.klevis.openrental.checkout/src/ramo/klevis/openrental/parts/checkout/PartSelectedCarInfo.java
@@ -1,7 +1,15 @@
 package ramo.klevis.openrental.parts.checkout;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 
 import org.eclipse.e4.core.di.annotations.Optional;
@@ -11,6 +19,7 @@ import org.eclipse.e4.ui.model.application.ui.menu.MToolControl;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
+import ramo.klevis.national.ibuisnesslogic.ICarsDao;
 import ramo.klevis.openrental.entity.Car;
 import ramo.klevis.openrental.forms.FormSelectedCar;
 import ramo.klevis.openrental.utils.CarImageHolder;
@@ -19,6 +28,7 @@ public class PartSelectedCarInfo {
 
 	private FormSelectedCar formSelectedCar;
 	Composite parent;
+	private CarImageHolder carImageHolder;
 
 	public PartSelectedCarInfo() {
 	}
@@ -29,13 +39,45 @@ public class PartSelectedCarInfo {
 
 		if (formSelectedCar != null)
 			formSelectedCar.setSelectedCar(new Car());
+
+		if (carImageHolder != null)
+			carImageHolder.dispose();
+		carImageHolder = new CarImageHolder(parent, 0);
+		carImageHolder.setBounds(1, 1, 300, 300);
 	}
 
 	@Inject
 	@Optional
-	public void showCSelectedCar(@EventTopic(value = "CarSelected") Car car) {
+	public void showSelectedCar(@EventTopic(value = "CarSelected") Car car) {
 
 		formSelectedCar.setSelectedCar(car);
+
+		showCarImage(car);
+
+	}
+
+	private void showCarImage(Car car) {
+		if (car.getPic() != null) {
+
+			if (car.getPic().length > 0) {
+				InputStream in = new ByteArrayInputStream(car.getPic());
+				try {
+					BufferedImage bImageFromConvert = ImageIO.read(in);
+					String extent = car.getExtent();
+					String string = "c:/prov/image." + extent;
+					if (extent != null) {
+						ImageIO.write(bImageFromConvert, extent, new File(
+								string));
+
+						carImageHolder.addImage(string);
+					}
+
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
 	}
 
 	/**
@@ -47,9 +89,9 @@ public class PartSelectedCarInfo {
 		this.parent = parent;
 		formSelectedCar = new FormSelectedCar(parent, SWT.NONE);
 
-		
-		CarImageHolder carImageHolder = new CarImageHolder(parent, 0);
+		carImageHolder = new CarImageHolder(parent, 0);
 		carImageHolder.setBounds(1, 1, 300, 300);
+
 	}
 
 	@PreDestroy
